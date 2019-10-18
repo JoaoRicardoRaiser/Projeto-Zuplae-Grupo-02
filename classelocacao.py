1d6c28c34cb3de42300e93e0925a6eb64fbdcdde
Tarcísio de Souza Neto <tarcisio.noop@gmail.com>
2019-10-18 11:26:22 -0300

    18/10/2019 - João

--- classelocacao.py
+++ classelocacao.py
@@ -1,14 +1,36 @@
 class Locacao:
     def __init__(self):
+        self.__nome = ''
         self.__id = 0
+        self.__valor = 0
         self.__id_cliente = 0
-        self.__id_filmes = 0
-        self.__id_valor = 0
+        self.__id_filme = 0
         self.__data_locacao = 0
+
+@property
+def nome(self):
+    return self.__nome
+
+@nome.setter
+def id(self,nome):
+    self.__nome = nome
+
+
+@property
+def valor(self):
+    return self.__valor
+
+@valor.setter
+def valor(self,valor):
+    self.__valor = valor
+
+
+
 
     @property
     def id(self):
         return self.__id
+
     @id.setter
     def id(self,id):
         self.__id = id
@@ -16,27 +38,31 @@ class Locacao:
     @property
     def id_cliente(self):
         return self.__id_cliente
+
     @id_cliente.setter
-    def id_cliente(self,id_cliente):
+    def id_cliente(self, id_cliente):
         self.__id_cliente = id_cliente
 
     @property
-    def id_filmes(self):
-        return self.__id_filmes
-    @id_filmes.setter
-    def __id_filmes(self,id_filmes):
-        self.__id_filmes = id_filmes
+    def id_filme(self):
+        return self.__id_filme
 
-    @property
-    def id_valor(self):
-        return self.__id_valor
-    @id_valor.setter
-    def id_valor(self,id_valor):
-        self.__id_valor = id_valor
+    @id_filme.setter
+    def id_filme(self, id_filme):
+        self.__id_filme = id_filme
+
 
     @property
     def data_locacao(self):
         return self.__data_locacao
+
     @data_locacao.setter
     def data_locacao(self,data_locacao):
         self.__data_locacao = data_locacao
+
+# class  Locacao:
+#     def __init__(self, id, idcliente, idfilme, datalocacao):
+#         self.id = id
+#         self.idcliente = idcliente
+#         self.idfilme = idfilme
+#         self.datalocacao = datalocacao
--- rotas.py
+++ rotas.py
@@ -35,6 +35,22 @@ def listar_clientes_db():  ############ MÉTODO PARA LISTAR OS DADOS DE CLIENTES
     conexao.close()
     return listar_clientes
 
+def listar_locacoes_db():  ############ MÉTODO PARA LISTAR OS DADOS DE LOCACOES JÁ CONTIDOS NA TABELA DO BD)
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae07", passwd="grupo02", database="zuplae07")
+    cursor = conexao.cursor()
+    cursor.execute("SELECT c.NOME, f.TITULO, f.VALOR, l.DATA_LOCACAO from LOCACAO as l join CLIENTE as c on l.ID_CLIENTE = c.ID join FILMES as f on l.ID_FILME = f.ID") 
+    listar_locacoes = []
+    for i in cursor.fetchall():
+        locacao  = Locacao()
+        locacao.nome = i[0]
+        locacao.id_filme = i[1]
+        locacao.valor = i[2]
+        locacao.data_locacao = i[3]
+        listar_locacoes.append(locacao)
+    conexao.close()
+    return listar_locacoes
+
+
 ############ MÉTODO PARA SALVAR FILMES NO BD ##############  
 def salvar_filmes_db(titulo, ano, valor):
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae07", passwd="grupo02", database="zuplae07")
@@ -52,6 +68,13 @@ def salvar_clientes_db(nome, telefone, cpf):
     conexao.commit()
     conexao.close()
 
+def salvar_locacao_db(id_cliente, id_filme, data_locacao):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae07", passwd="grupo02", database="zuplae07")
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO LOCACAO (ID_CLIENTE, ID_FILME, DATA_LOCACAO)" + 
+    " VALUES ('{}', '{}', '{}')".format(id_cliente, id_filme, data_locacao))
+    conexao.commit()
+    conexao.close()
 
 
 
@@ -82,6 +105,8 @@ def clientes():
 def filmes_cadastro():
     return render_template ('cadastrar_filmes.html')
  
+
+################### ROTA PARA SALVAR NO BANCO DE DADOS ################# 
 @app.route('/filmes/salvar', methods = ["POST"])
 def salvar():
     titulo = request.form['titulo']
@@ -107,6 +132,24 @@ def salvar_clientes():
     salvar_clientes_db(cliente.nome, cliente.telefone, cliente.cpf)
     return redirect('/clientes')
 
+
+@app.route('/locacao/salvar', methods = ["POST"])
+def salvar_locacao():
+    filmes = request.form['filmes']
+    cliente = request.form['cliente']
+    data_locacao = request.form['data_locacao']
+    locacao= Locacao()
+    locacao.id_filmes = filmes
+    locacao.id_cliente = cliente
+    locacao.data_locacao = data_locacao
+    salvar_locacao_db(locacao.id_filmes, locacao.id_cliente, locacao.data_locacao)
+    return redirect('/locacoes')
+
+
+
+
+
+
 @app.route('/clientes/cadastro')
 def cliente_cadastro():
     return render_template('cadastrar_clientes.html')
@@ -114,7 +157,8 @@ def cliente_cadastro():
 
 @app.route('/locacoes')
 def locacoes():
-    return render_template('lista_locacoes.html')
+    lista3 = listar_locacoes_db()
+    return render_template('lista_locacoes.html', lista3 = lista3)
 
 @app.route('/locar/filme')
 def locar_filme():
@@ -122,7 +166,7 @@ def locar_filme():
     listar_titulos = listar_filmes_db()
     return render_template('cadastrar_locacao.html', lista = listar_nomes, lista2 = listar_titulos)
 
-app.run(debug=True)
+app.run()
 
 
 
